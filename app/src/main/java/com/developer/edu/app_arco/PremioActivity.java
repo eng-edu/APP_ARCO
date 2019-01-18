21c5baa545437432a074a3fa4307928129123a09
eng-edu <eduardo.eng15@hotmail.com>
2019-01-17 23:54:55 -0200

    up

---
+++ app/src/main/java/com/developer/edu/app_arco/PremioActivity.java
@@ -0,0 +1,13 @@
+package com.developer.edu.app_arco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PremioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_premio);
+    }
+}
